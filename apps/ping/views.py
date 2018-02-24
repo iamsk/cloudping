52a72465202733487a20f8939d0466c5341f53e4
Bin <iamsk.info@gmail.com>
2018-02-24 16:46:38 +0800

    change company order

--- apps/ping/views.py
+++ apps/ping/views.py
@@ -6,19 +6,23 @@ from django.db.models import Count
 from .models import Service, Company, Region
 
 
+def get_companies():
+    rs = Service.objects.filter(status=0).values('company').annotate(total=Count('company')).order_by('-total')
+    companies, counts = [], []
+    for r in rs:
+        companies.append(Company.objects.get(pk=r['company']))
+        counts.append(r['total'])
+    return companies, counts
+
+
 class IndexView(TemplateView):
     template_name = "cloudping.html"
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
-        context['cs'] = Company.objects.order_by('order').all()
-        context['company'] = 'all'
-        rs = Service.objects.filter(status=0).values('company').annotate(total=Count('company')).order_by('-total')
-        companies, counts = [], []
-        for r in rs:
-            companies.append(Company.objects.get(pk=r['company']))
-            counts.append(r['total'])
+        companies, counts = get_companies()
         context['companies'] = companies
+        context['company'] = 'all'
         context['counts'] = counts
         context['rows'] = self.get_rows(companies)
         context['description'] = u'主流云平台地域分布及其延迟'
@@ -41,7 +45,8 @@ class CompanyView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(CompanyView, self).get_context_data(**kwargs)
-        context['cs'] = Company.objects.order_by('order').all()
+        companies, _ = get_companies()
+        context['companies'] = companies
         context['company'] = company = Company.objects.filter(code=kwargs['code']).first()
         context['ss'] = Service.objects.filter(company=context['company'], status=0).order_by('region__order')
         context['description'] = company.description
