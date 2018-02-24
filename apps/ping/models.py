7815c7f9ac1477413873582252c212d15fabca3e
Bin <iamsk.info@gmail.com>
2018-02-24 16:47:51 +0800

    add en version

--- apps/ping/models.py
+++ apps/ping/models.py
@@ -2,25 +2,25 @@ from django.db import models
 
 
 class Company(models.Model):
-    name_cn = models.CharField(max_length=30, null=True, blank=True)
     name_en = models.CharField(max_length=30, null=True, blank=True)
+    name_cn = models.CharField(max_length=30, null=True, blank=True)
     code = models.CharField(max_length=30)
     link = models.CharField(max_length=300)
     order = models.IntegerField(default=0)
     description = models.TextField(null=True, blank=True)
 
     def __unicode__(self):
-        return self.name_cn or self.name_en
+        return self.name_en or self.name_cn
 
 
 class Region(models.Model):
-    name_cn = models.CharField(max_length=30, null=True, blank=True)
     name_en = models.CharField(max_length=30, null=True, blank=True)
+    name_cn = models.CharField(max_length=30, null=True, blank=True)
     order = models.IntegerField(default=0)
     description = models.TextField(null=True, blank=True)
 
     def __unicode__(self):
-        return self.name_cn or self.name_en
+        return self.name_en or self.name_cn
 
 
 class Service(models.Model):
