dfc02a81949c7de4cbcab6e30a67660c0c93a515
Bin <iamsk.info@gmail.com>
2018-12-13 23:23:05 +0800

    length of thumbnail

--- apps/promotion/models.py
+++ apps/promotion/models.py
@@ -7,7 +7,7 @@ class Promotion(models.Model):
     title = models.CharField(max_length=100)
     company = models.ForeignKey(Company)
     link = models.CharField(max_length=300)
-    thumbnail = models.CharField(max_length=100, null=True, blank=True)
+    thumbnail = models.CharField(max_length=300, null=True, blank=True)
     order = models.IntegerField(default=0)
     description = models.TextField(null=True, blank=True)
 
